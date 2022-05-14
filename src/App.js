cf1cc345b025efbed337bd87881b4e07ad9a7408
Luis Dudamel <ladusa@gmail.com>
2022-05-14 21:14:19 +0200

    First box height sizing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  border: solid 2px red;
+`;
+
 function App() {
-  return <div className="App">IN CONSTRUCCION, CAPO</div>;
+  return (
+    <MainContainer>
+      <Container fluid className="vh-100">
+        <Row>
+          <Col>EN CONSTRUCCION CAPO</Col>
+          <Col>EN CONSTRUCCION CAPO</Col>
+        </Row>
+      </Container>
+    </MainContainer>
+  );
 }
 
 export default App;
---
+++ src/components/RobotsList/RobotsList.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const RobotsListContainer = styled.div``;
+
+export const RobotsList = () => {};
