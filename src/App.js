2c36204b9f170071dc3af0eebb3d4a6cedd1c193
Luis Dudamel <ladusa@gmail.com>
2022-05-15 13:38:23 +0200

    Created grid robot card layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
-import styled from "styled-components";
 import { RobotsList } from "./components/RobotsList/RobotsList";
 import { loadRobotsThunk } from "./redux/thunks/robotsThunks";
-
-const MainContainer = styled.div`
-  border: solid 2px red;
-`;
 
 function App() {
   const dispatch = useDispatch();
@@ -17,13 +12,9 @@ function App() {
   }, [dispatch]);
 
   return (
-    <MainContainer>
-      <Container fluid className="vh-100">
-        <Row>
-          <RobotsList></RobotsList>
-        </Row>
-      </Container>
-    </MainContainer>
+    <Container fluid className="vh-100">
+      <RobotsList></RobotsList>
+    </Container>
   );
 }
 
